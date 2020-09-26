d5f77f3e795a3d6a798204e613c727e5e3eb6a9c
yangjian <362961910@qq.com>
2020-09-26 22:52:37 +0800

    complete more

--- spring-cloud-app/spring-cloud-service-cluster/service-core/src/main/java/com/jy/study/springcloud/serviceremote/User.java
+++ spring-cloud-app/spring-cloud-service-cluster/service-core/src/main/java/com/jy/study/springcloud/serviceremote/User.java
@@ -1,4 +1,4 @@
-package com.jy.study.springcloud.serviceremote.domain;
+package com.jy.study.springcloud.serviceremote;
 
 public class User {
     private Long id;
--- spring-cloud-app/spring-cloud-service-cluster/service-remote/src/main/java/com/jy/study/springcloud/serviceremote/UserServiceRemote.java
+++
@@ -1,7 +0,0 @@
-package com.jy.study.springcloud.serviceremote;
-
-import com.jy.study.springcloud.serviceremote.domain.User;
-
-public interface UserServiceRemote {
-    User getById(Long id);
-}
