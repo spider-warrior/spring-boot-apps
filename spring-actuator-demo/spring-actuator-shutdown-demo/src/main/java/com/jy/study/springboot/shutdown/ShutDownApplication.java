d8dfe266366c77ddba65aa7d1610405dc462d709
yangjian <362961910@qq.com>
2020-10-13 16:21:16 +0800

    springboot实现优雅关机

---
+++ spring-actuator-demo/spring-actuator-shutdown-demo/src/main/java/com/jy/study/springboot/shutdown/ShutDownApplication.java
@@ -0,0 +1,11 @@
+package com.jy.study.springboot.shutdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShutDownApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShutDownApplication.class, args);
+    }
+}
